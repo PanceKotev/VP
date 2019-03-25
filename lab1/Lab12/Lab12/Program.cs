37c82bf9cd81ef031ae35d41ffbbdbd27c98da6b
Pance Kotev <48929662+PanceKotev@users.noreply.github.com>
2019-03-26 00:17:46 +0100

    Add files via upload

---
+++ lab1/Lab12/Lab12/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napisete kolku broevi sakate da vnesuvate");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            Console.WriteLine("Dali sakate na random ili sakate da vnesuvate broevi? (random za random, ne za vnesuvanje)");
+            string izbor=Console.ReadLine().ToLower();
+            bool tocno = true;
+            int[] broevi = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                if (izbor.Equals("ne")) {
+                    Console.Write("{0}: ", i + 1);
+                    broevi[i] = Convert.ToInt32(Console.ReadLine()); }
+                else if(izbor.Equals("random"))
+                {
+                    broevi[i] = random.Next(1, 1000);
+                   Console.Write("{0}: {1} \n",i + 1,broevi[i]);
+                }
+                else
+                {
+                    Console.Write("Vnesovte nevaliden izbor\n");
+                    tocno = false;
+                    break;
+                }
+                }
+            int min = 0;
+            minbroj(broevi, out min);
+            int max = 0;
+            maxbroj(broevi, out max);
+            if (tocno)
+            {
+                Console.WriteLine("Srednata vrednost e: {0:0.00}\n", Srednavred(broevi));
+                Console.WriteLine("Najgolemiot broj e: {0}\n",min );
+                Console.WriteLine("Najmaliot broj e: {0}\n",max);
+            }
+            Console.ReadLine();
+        }
+        static double Srednavred( int[] br)
+        {
+            double zbir = 0;
+            foreach(int b in br)
+            {
+                zbir += b;
+            }
+            zbir = zbir/br.Length;
+            return zbir;
+        }
+        static void minbroj(int[] br,out int min)
+        {
+            min = Int32.MaxValue;
+            foreach(int b in br)
+            {
+                if (min > b)
+                    min = b;
+            }
+        }
+        static void maxbroj(int[] br,out int max)
+        {
+            max = Int32.MinValue;
+            foreach (int b in br)
+            {
+                if (max < b)
+                   max = b;
+            }
+        }
+    }
+}
---
+++ lab1/Lab15/Lab15/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    enum PasswordStrength : int
+    {
+        easy=1,
+        normal=2,
+        hard=3,
+        unknown=4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Na koja teskost sakas da pogoduvas password?(easy,normal,hard)\n");
+            string izb= Console.ReadLine();
+            PasswordStrength izbor;
+            if (izb.Equals("easy"))
+                izbor = PasswordStrength.easy;
+            else if (izb.Equals("normal"))
+                izbor = PasswordStrength.normal;
+            else if (izb.Equals("hard"))
+                izbor = PasswordStrength.hard;
+            else
+                izbor = PasswordStrength.unknown;
+            string rand = generatePassword(izbor);
+            Console.WriteLine(rand);
+            string passLine = Console.ReadLine();
+            char[] delimiter = { ' ' };
+            string[] parts = passLine.Split(delimiter);
+            bool tocno = false;
+            foreach(string del in parts)
+            {
+                if(del.Equals(rand))
+                {
+                    tocno = true;
+                    break;
+                }
+
+            }
+            if (tocno)
+                Console.WriteLine("Bravo! ja pogodivte lozinkata {0} ", rand);
+            else
+                Console.WriteLine("Ne ja pogodivte lozinkata {0}",rand);
+            Console.ReadLine();
+
+
+        }
+        static string generatePassword(PasswordStrength passwordStrength)
+        {
+            int sila = (int)passwordStrength;
+            Random random = new Random();
+            string pogodi = "";
+            if (sila == 1) {
+                char[] source = "abcdefghijklmnoprqstuvwxyz".ToCharArray();
+                int n = random.Next(1, 6);
+                for (int i = 0; i < n; i++)
+                {
+                    pogodi += source[random.Next(0, source.Length-1)];
+                }
+            }
+            else if (sila == 2)
+            {
+               char[] source= "abcdefghijklmnoprqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+                int n = random.Next(6, 10);
+                for(int i = 0; i < n; i++)
+                {
+                    pogodi += source[random.Next(0, source.Length-1)];
+                }
+            }
+            else if (sila == 3)
+            {
+                char[] source = "abcdefghijklmnoprqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%^&*()_+-=][;,.\\/`~<>".ToCharArray();
+                int n = random.Next(10, 25);
+                for (int i = 0; i < n; i++)
+                {
+                    pogodi += source[random.Next(0, source.Length - 1)];
+                }
+            }
+            return pogodi;
+        }
+    }
+}
